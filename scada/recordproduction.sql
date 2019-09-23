90ce4756e7bf342ec86c65cddecf5735d3e4c19a
Brent Groves <brent.groves@gmail.com>
2019-09-23 15:38:35 -0400

    Added record production

--- scada/recordproduction.sql
+++ scada/recordproduction.sql
@@ -8,40 +8,69 @@ select top 100 * from part_v_job_status  --DON'T KNOW HOW TO RELATE THIS TO JOB
 select top 100 * from part_v_operator  -- DON'T KNOW HOW TO LINK THIS TO NAME
 select top 100 * from part_v_part_operation  -- DON'T KNOW HOW THIS RELATES TO JOB OPERATION.
 select top 100 * from part_v_production
-
+select top 100 * from part_v_setup
 
 /*
-part job production
+work centers production records
 */
 
-DECLARE @PLC_Name VARCHAR(50)
-set @PLC_Name = 'CNC103'
+--DECLARE @PLC_Name VARCHAR(50)
+--set @PLC_Name = 'CNC103'
+DECLARE @Workcenter_code VARCHAR(50)
+set @Workcenter_code = 'CNC103'
 
 
 select top 100 
-pr.setup_key,pr.tare_weight,
-pr.quantity,pr.record_date,pr.record_by,
-u.last_name,u.first_name,
 w.workcenter_code,w.plc_name,w.name,
-j.job_no,o.op_no,j.quantity,j.due_date,
-o.description,
-s.job_status,
-p.part_no,p.name
-from part_v_production pr
-left outer join part_v_workcenter AS W
-on pr.workcenter_key=w.workcenter_key
+s.setup_key,s.setup_date,
+c.serial_no,c.container_status,
+p.part_no,p.name,
+po.operation_no,po.standard_quantity,Standard_Container_Type,
+j.job_no,j.quantity,j.due_date,
+jo.op_no,
+--pr.tare_weight,pr.quantity,pr.record_date,
+--u.last_name,u.first_name,
+js.job_status
+from part_v_workcenter AS W
+left outer join part_v_setup s
+on w.workcenter_key=s.workcenter_key
+left outer join part_v_setup_container c  
+on s.setup_key=c.setup_key
+left outer join part_v_part p  
+on s.part_key=p.part_key
+left outer join part_v_part_operation po  
+on s.part_operation_key=po.part_operation_key
+--left outer join Part_Container_Type ct
+--on po.
+left outer join part_v_job_op jo --each production record is for a specific job operation.
+on s.job_op_key=jo.job_op_key
+left outer join part_v_job j 
+on jo.job_key = j.job_key
+left outer join part_v_job_status js
+on j.job_status_key=js.job_status_key
+left outer join part_v_production pr 
+on s.setup_key=pr.setup_key
 left outer join Plexus_Control_v_Plexus_User u
 on pr.record_by=u.plexus_user_no
-left outer join part_v_part p  --WHY ARE THESE NOT LINKING TO THE WORK CENTER?
-on pr.part_key=p.part_key
-left outer join part_v_job_op o --each production record is for a specific job operation.
-on pr.job_op_key=o.job_op_key
-left outer join part_v_job j 
-on o.job_key = j.job_key
-left outer join part_v_job_status s
-on j.job_status_key=s.job_status_key
-where w.plc_name = @PLC_Name 
+where w.workcenter_code = @Workcenter_code 
 and pr.record_date > '9/23/2019 6:00:00 AM'
+
+
+--DECLARE @Workcenter_code VARCHAR(50)
+--set @Workcenter_code = 'CNC103'
+
+select top 100 
+w.workcenter_code,w.plc_name,w.name,
+s.setup_key,s.setup_date,
+c.container_status,c.serial_no
+from part_v_workcenter AS W
+left outer join part_v_setup s
+on w.workcenter_key=s.workcenter_key
+left outer join part_v_setup_container c  
+on s.setup_key=c.setup_key
+where w.workcenter_code = @Workcenter_code 
+--and pr.record_date > '9/23/2019 6:00:00 AM'
+
 
 
 
@@ -50,4 +79,5 @@ where production_date >= '9/23/2019 06:00:00'
 select top 100 * from part_v_container
 where location like '%CNC 103%'
 select top 100 * from part_v_container_status
+
 
