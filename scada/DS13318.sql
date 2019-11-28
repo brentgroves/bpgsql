ef7376cd8df3dfa37f5cfb7e1fff0b2cb71699ca
Brent Groves <brent.groves@gmail.com>
2019-11-28 11:13:19 -0500

    changed/added code

--- scada/DS13318.sql
+++ scada/DS13318.sql
@@ -1,16 +1,15 @@
+
+
+CALL DS13318('2019-11-27 13:30');
+
 select 
-/*
-count(*)
-*/
 TransDate,Part_no,Serial_No,ProdServer,Quantity,Container_Status 
 from DS13318
-where TransDate >= "2019-11-25 16:00:00"
-/*where ProdServer = 1*/
-order by TransDate desc,Part_no,Serial_no
+where TransDate >= '2019-11-27 13:01:00' 
+order by TransDate,Part_no,Serial_no;
 
-/*
-delete from DS13318 where TransDate > "2019-11-21"
- */
-*/
-
-select * from DS13318 where TransDate >= "2019-11-25 16:00:00"
+select 
+count(*)
+from DS13318
+where TransDate >= '2019-11-27 13:01:00' 
+order by TransDate,Part_no,Serial_no;
