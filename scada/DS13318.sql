dba3883af8c230354916fe9260066fbdd8099bae
Brent Groves <brent.groves@gmail.com>
2019-12-04 11:38:17 -0500

    Added/Changed code

--- scada/DS13318.sql
+++ scada/DS13318.sql
@@ -1,7 +1,10 @@
 
 
-CALL DS13318('2019-11-30 10:30');
+CALL DS13318('2019-12-02 09:15');
+CALL DS13318('2019-12-15 09:00');
 
+select TransDate,Part_no,Serial_No,ProdServer,Quantity,Container_Status from DS13318 where TransDate = '2019-12-15 09:00'
+delete from DS13318 where TransDate = '2019-12-15 09:00'
 select 
 TransDate,Part_no,Serial_No,ProdServer,Quantity,Container_Status 
 from DS13318
@@ -13,3 +16,8 @@ count(*)
 from DS13318
 where TransDate >= '2019-11-27 13:01:00' 
 order by TransDate,Part_no,Serial_no;
+
+select CONVERT_TZ(TransDate,'+00:00','-5:00')TransDate2,
+DATE_FORMAT(TransDate, "%W %M %e %Y"),
+TransDate,Part_no,Workcenter,Serial_No,ProdServer,Quantity,Container_Status 
+from DS13318
--- scada/Mach2Schema.sql
+++ scada/Mach2Schema.sql
@@ -4,6 +4,8 @@ CREATE TABLE DS13318(
   TransDate datetime DEFAULT NULL,
   PCN varchar(50) NULL,
   ProdServer bool NULL,
+  Workcenter varchar(50) NULL,
+  CNC varchar(25) NULL,
   Part_No varchar(50) NULL,
   Name varchar(50) NULL,
   Multiple bool NULL,
@@ -40,15 +42,51 @@ CREATE TABLE DS13318(
 )  ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COMMENT='Control_Panel_Setup_Containers_Get_Key historian';
 
 DROP PROCEDURE DS13318;
- 
+
 CREATE PROCEDURE DS13318 (
-    IN  transDate VARCHAR(25)
+    IN  p_TransDate VARCHAR(25)
 )
 BEGIN
+	
+/* set @p_TransDate = '2019-12-15 09:00'; */
+drop temporary table if exists ProdServer;
+create temporary table ProdServer engine=memory	
 select 
-TransDate,Part_no,Serial_No,ProdServer,Quantity,Container_Status 
-from DS13318
-where TransDate = transDate 
-order by TransDate,Part_no,Serial_no;
+TransDate,Workcenter,CNC,Name,Part_no,Serial_No,ProdServer,Quantity,Container_Status 
+from DS13318 
+where TransDate = p_TransDate and ProdServer = 1; 
+
+drop temporary table if exists TestServer;
+create temporary table TestServer engine=memory	
+select 
+TransDate tst_TransDate,Workcenter tst_Workcenter,CNC tst_CNC,Name tst_Name, Part_no tst_Part_No,Serial_No tst_Serial_No,ProdServer tst_ProdServer,Quantity tst_Quantity,Container_Status tst_Container_Status
+from DS13318 
+where TransDate = p_TransDate and ProdServer = 0; 
+
+select 
+TransDate,Workcenter,CNC,Name,Part_no,Serial_No,tst_Serial_No, Quantity,tst_Quantity, Container_Status,
+case 
+when Serial_No <> tst_Serial_No then 'Red'
+when ((Serial_No = tst_Serial_No) and (Quantity <> tst_Quantity)) then 'Yellow'
+when ((Serial_No = tst_Serial_No) and (Quantity = tst_Quantity)) then 'Green'
+when ((Serial_No = tst_Serial_No) and (Quantity is Null) and (tst_Quantity is Null)) then 'Green'
+end Status
+from ProdServer ps
+left outer join TestServer ts
+on ps.TransDate = ts.tst_TransDate
+and ps.Workcenter = ts.tst_Workcenter
+and ps.Part_No = ts.tst_Part_No
+and ps.Container_Status = ts.tst_Container_Status
+order by ps.TransDate,ps.Part_no,ps.Serial_no,ps.Container_Status;
+
 END;
 
+
+CALL DS13318('2019-12-15 09:00');
+drop PROCEDURE test1
+CREATE PROCEDURE test1 ()
+BEGIN
+	select * from DS13318;
+END;
+call test1
+SELECT CONVERT_TZ('2004-01-01 12:00:00','+00:00','+10:00');
