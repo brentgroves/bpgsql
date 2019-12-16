bb9ec4857ff3703d6306430ac91aeee0caca407c
Brent Groves <brent.groves@gmail.com>
2019-12-16 08:56:25 -0500

    Resolved merge conflict by incorporating both suggestions.

--- MRO/ItemLocationDiff.sql
+++ MRO/ItemLocationDiff.sql
@@ -1,7 +1,8 @@
---drop table station1203
+--drop table station1209
 select * 
-into station1209
+into station1216
 from STATION
+--12674
 --12673
 --12677
 --12676
@@ -14,11 +15,11 @@ from STATION
 --12653
 --12624
 --verify backup of station
-select top 100 * from station1209
+select top 100 * from station1216
 --12653
 -- Upload the item_location table into PlxSupplyItemLocation table.
 --drop table dbo.PlxSupplyItemLocation1203
-CREATE TABLE Cribmaster.dbo.PlxSupplyItemLocation1209 (
+CREATE TABLE Cribmaster.dbo.PlxSupplyItemLocation1216 (
 	item_no varchar(50),
 	location varchar(50),
 	quantity integer
@@ -26,11 +27,11 @@ CREATE TABLE Cribmaster.dbo.PlxSupplyItemLocation1209 (
 --update purchasing.dbo.item set Description=Brief_Description + ', ' + Description where Brief_Description <> Description
 -- Verify table was created and has zero records
 --drop table PlxSupplyItemLocation1125
-select count(*) from PlxSupplyItemLocation1209  --
+select count(*) from PlxSupplyItemLocation1216  --
 -- truncate table PlxSupplyItemLocation0730
 -- Insert Plex item_location data into CM
-Bulk insert PlxSupplyItemLocation1209
-from 'c:\il1209GE12500.csv'
+Bulk insert PlxSupplyItemLocation1216
+from 'c:\il1216gE12500.csv'
 with
 (
 	fieldterminator = ',',
@@ -40,7 +41,8 @@ with
 select
 count(*)
 --top 1000 * 
-from PlxSupplyItemLocation1209 --0
+from PlxSupplyItemLocation1216 --0
+--13283
 --13285
 --13260
 --13258
@@ -61,18 +63,18 @@ from PlxSupplyItemLocation1209 --0
  * Plex '01-002A01' Item locations with quantity = 0: 2534
  * Plex '01-002A01' Item locations with quantity <> 0: 7
  */
---drop table dbo.nic1203
+--drop table dbo.nic1209
 
 select 
 il.item_no
 --il.item_no,il.location,il.quantity
-into nic1209 --Plex supply items with the default location and a quantity = 0
---count(*) --2581,2563,2563,2563,2562,2561,236	
+into nic1216 --Plex supply items with the default location and a quantity = 0
+--count(*) --2577,2581,2563,2563,2563,2562,2561,236	
 --il.item_no,inv.ItemClass,inv.Description1,il.location,il.quantity as PlexQuantity,st.BinQuantity as CribMasterQty,st.Quantity as CMQuantity
 from (
 	select --distinct incase I inserted items more than once
 		distinct item_no,location,quantity
-	from PlxSupplyItemLocation1209 
+	from PlxSupplyItemLocation1216 
 ) il
 left outer join STATION st 
 on il.location=st.CribBin
@@ -89,7 +91,8 @@ and il.quantity = 0
 --and il.quantity <> 0 
 
 	select COUNT(*)
-	from nic1209
+	from nic1216
+	--2577
 	--2581
 	--2563
 	--2563
@@ -119,9 +122,9 @@ Quantity = 0
 	item in 
 	(
 	select item_no
-	from nic1209
+	from nic1216
 	)
-	and (st.BinQuantity<>0 or st.Quantity <> 0 ) --12,12,13,13,15,12,13,12, 10, 8,9,10
+	and (st.BinQuantity<>0 or st.Quantity <> 0 ) --12,12,12,13,13,15,12,13,12, 10, 8,9,10
 	--and (st.BinQuantity=0 and st.Quantity = 0 )  --1960,1963
 
 
@@ -137,16 +140,16 @@ Quantity = il.quantity
 from (
 	select --distinct incase I inserted items more than once
 		distinct item_no,location,quantity
-	from PlxSupplyItemLocation1209 
+	from PlxSupplyItemLocation1216 
 ) il
 inner join STATION st 
 on il.location=st.CribBin
 and il.item_no=st.Item
 --0729=10630, 0726=10630, --0628=11285
 where 
-il.quantity <> st.BinQuantity --510,293,376,416,417,472, 342,353, 455,406,384	
---il.quantity > st.BinQuantity --134,140,171,172,177,120,138,131,61
---il.quantity < st.BinQuantity --376,168,236,245,245,295,222,311,215, 316,275,105
+il.quantity <> st.BinQuantity --1319,510,293,376,416,417,472, 342,353, 455,406,384	
+--il.quantity > st.BinQuantity --538,134,140,171,172,177,120,138,131,61
+--il.quantity < st.BinQuantity --781,376,168,236,245,245,295,222,311,215, 316,275,105
 --80 more items dropped in quantity 0820
 --385
 --0813=389
--- kors/KorsSchema.sql
+++ kors/KorsSchema.sql
@@ -1,13 +1,11 @@
 -- Drop table
 
--- DROP TABLE master.dbo.CommandLog GO
-
+-- drop TABLE Kors.dbo.HourlyOEEValues
 CREATE TABLE Kors.dbo.HourlyOEEValues (
 	ID int IDENTITY (1,1) NOT NULL,
 	Workcenter_Code varchar(50),
 	Job_number varchar(20),
 	Part_number varchar(60),
-	Workcenter_status varchar(50),
 	Data_hour INT,
 	Hourly_planned_production_count INT,
 	Hourly_actual_production_count INT,
@@ -15,9 +13,8 @@ CREATE TABLE Kors.dbo.HourlyOEEValues (
 	Cumulative_actual_production_count INT,
 	scrap_count INT,
 	Downtime_minutes INT,
-	Date_time_stamp DATETIME,
-	Transaction_number BIGINT
-) GO
+	Date_time_stamp DATETIME
+) 
 
 select * from hourlyoeevalues
 declare @dt datetime;
@@ -30,7 +27,6 @@ CREATE PROCEDURE InsertHourlyOEEValues
 	@Workcenter_Code varchar(50),
 	@Job_number varchar(20),
 	@Part_number varchar(60),
-	@Workcenter_status varchar(50),
 	@Data_hour INT,
 	@Hourly_planned_production_count INT,
 	@Hourly_actual_production_count INT,
@@ -38,8 +34,7 @@ CREATE PROCEDURE InsertHourlyOEEValues
 	@Cumulative_actual_production_count INT,
 	@scrap_count INT,
 	@Downtime_minutes INT,
-	@Date_time_stamp DATETIME,
-	@Transaction_number BIGINT
+	@Date_time_stamp DATETIME
 AS
 BEGIN
     -- SET NOCOUNT ON added to prevent extra result sets from
@@ -51,10 +46,10 @@ DECLARE @MyTableVar table( ID int,
                            Workcenter_Code varchar(50));
    
 INSERT INTO Kors.dbo.HourlyOEEValues
-(Workcenter_Code, Job_number, Part_number, Workcenter_status, Data_hour, Hourly_planned_production_count, Hourly_actual_production_count, Cumulative_planned_production_count, Cumulative_actual_production_count, scrap_count, Downtime_minutes, Date_time_stamp, Transaction_number)
+(Workcenter_Code, Job_number, Part_number, Data_hour, Hourly_planned_production_count, Hourly_actual_production_count, Cumulative_planned_production_count, Cumulative_actual_production_count, scrap_count, Downtime_minutes, Date_time_stamp)
 OUTPUT INSERTED.ID, INSERTED.Workcenter_Code
 into @MyTableVar
-VALUES(@Workcenter_Code, @Job_number, @Part_number, @Workcenter_status, @Data_hour, @Hourly_planned_production_count, @Hourly_actual_production_count, @Cumulative_planned_production_count, @Cumulative_actual_production_count, @scrap_count, @Downtime_minutes, @Date_time_stamp, @Transaction_number);
+VALUES(@Workcenter_Code, @Job_number, @Part_number, @Data_hour, @Hourly_planned_production_count, @Hourly_actual_production_count, @Cumulative_planned_production_count, @Cumulative_actual_production_count, @scrap_count, @Downtime_minutes, @Date_time_stamp);
 --values (' VSC_5', '1210', '4140', 'Production', 10, 41, 38, 834,582, 0, 0,'2014-07-02 14:29', 0)
 
 --Display the result set of the table variable.
