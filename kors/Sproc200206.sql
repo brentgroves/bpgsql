5c94d3bf9fbd076c884982d7102e757adef99ef1
Brent Groves <brent.groves@gmail.com>
2020-02-08 18:07:00 -0500

    Added/Update source code

--- kors/Sproc200206.sql
+++ kors/Sproc200206.sql
@@ -3,14 +3,19 @@ select top 100 * from HourlyOEEValues
 CREATE PROCEDURE Sproc200206
 	@start_date DATETIME,
 	@end_date DATETIME,
-	@report_name varchar(12)
+	@table_name varchar(12),
+	@record_count INT OUTPUT
 AS
 BEGIN
     -- SET NOCOUNT ON added to prevent extra result sets from
     -- interfering with SELECT statements.
     SET NOCOUNT ON;
+	IF OBJECT_ID(@table_name) IS NOT NULL
+    	EXEC ('DROP Table ' + @table_name)
 
-EXEC('select top(10) * into ' + @report_name + ' from HourlyOEEValues')
+    EXEC('select top(10) * into ' + @table_name + ' from HourlyOEEValues')
+    
+    SELECT @record_count = @@ROWCOUNT;
 --EXEC('select * from ' + @report_name)
    
 END;
@@ -19,18 +24,19 @@ END;
 DECLARE	@return_value int,
 @start_date DATETIME,
 @end_date DATETIME,
-@report_name varchar(12);
+@table_name varchar(12),
+@record_count int;
 --2004-05-23T14:25:10
 set @start_date ='2019-12-15T09:00:00';
 set @end_date ='2019-12-15T09:00:00';
-set @report_name = 'rpt0207558'
-EXEC	@return_value = [dbo].[Sproc200206] @start_date,@end_date,@report_name
-
+set @table_name = 'rpt02080'
+EXEC	@return_value = [dbo].[Sproc200206] @start_date,@end_date,@table_name,@record_count OUTPUT
+select '@record_count' = @record_count
 SELECT	'Return Value' = @return_value
 
 GO
-
-select * from rpt0207558
+--drop table rpt02080
+select * from rpt02080
 
 SELECT * 
 FROM rpt0207558
