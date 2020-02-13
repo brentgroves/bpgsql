764a977b7683a75fa870e70382639fc4bffffda1
Brent Groves <brent.groves@gmail.com>
2020-02-13 16:14:38 -0500

    Added/Update source code

--- kors/Sproc200206.sql
+++ kors/Sproc200206.sql
@@ -1,27 +1,3 @@
-select top 100 * from HourlyOEEValues 
---drop procedure Sproc200206 
-CREATE PROCEDURE Sproc200206
-	@start_date DATETIME,
-	@end_date DATETIME,
-	@table_name varchar(12),
-	@record_count INT OUTPUT
-AS
-BEGIN
-    -- SET NOCOUNT ON added to prevent extra result sets from
-    -- interfering with SELECT statements.
-    SET NOCOUNT ON;
-	IF OBJECT_ID(@table_name) IS NOT NULL
-    	EXEC ('DROP Table ' + @table_name)
-
-    EXEC('select top(1000) * into ' + @table_name + ' from HourlyOEEValues where Date_time_stamp BETWEEN ' + @start_date + ' and ' + @end_date )
-    
-    SELECT @record_count = @@ROWCOUNT;
-
---EXEC('select * from ' + @report_name)
-   
-END;
-
---drop procedure Sproc200206 
 CREATE PROCEDURE Sproc200206
 	@start_date DATETIME,
 	@end_date DATETIME,
@@ -36,121 +12,16 @@ BEGIN
 	IF OBJECT_ID(@table_name) IS NOT NULL
     	EXEC ('DROP Table ' + @table_name)
 
---    EXEC('select top(1000) * into ' + @table_name + ' from HourlyOEEValues where Date_time_stamp BETWEEN ' + @start_date + ' and ' + @end_date )
- --   EXEC('select * from HourlyOEEValues where Date_time_stamp BETWEEN ''' + CONVERT(VARCHAR(10),@start_date, 101) + ''' and ''' + CONVERT(VARCHAR(10),@end_date, 101) + '''')
 --	select * from HourlyOEEValues where Date_time_stamp BETWEEN @start_date and @end_date
-
-select @sql = N'SELECT * into ' + quotename(@table_name) + N' from hourlyoeevalues  WHERE Date_time_stamp BETWEEN cast(floor(cast(@fromDate as float)) as datetime) AND cast(floor(cast(@toDate as float)) as datetime)'
+-- use floor for date only comparison
+--select @sql = N'SELECT * into ' + quotename(@table_name) + N' from hourlyoeevalues  WHERE Date_time_stamp BETWEEN cast(floor(cast(@fromDate as float)) as datetime) AND cast(floor(cast(@toDate as float)) as datetime)'
+select @sql = N'SELECT * into ' + quotename(@table_name) + N' from hourlyoeevalues  WHERE Date_time_stamp BETWEEN @fromDate AND @toDate'
 
 EXEC sp_executesql @sql, N'@fromDate datetime, @toDate datetime', @start_date, @end_date
     
 SELECT @record_count = @@ROWCOUNT;
---EXEC('select * from ' + @report_name)
    
 END;
-
-
-DECLARE	@return_value int,
-@start_date DATETIME,
-@end_date DATETIME,
-@table_name varchar(12),
-@record_count int;
---2004-05-23T14:25:10
---YYYYMMDD or YYYY-MM-DD
---YYYY-MM-DDThh:mm:ss.nnn
-set @start_date ='2020-02-09T00:00:00';
---select @start_date
-set @end_date ='2020-02-10T23:59:59';
---HH:MM:SS.SSS
-set @table_name = 'rpt02080';
-EXEC	@return_value = [dbo].[Sproc200206] @start_date,@end_date,@table_name,@record_count OUTPUT
-select '@record_count' = @record_count
-SELECT	'Return Value' = @return_value
-
---drop procedure SprocTest
-CREATE PROCEDURE SprocTest
-	@start_date DATETIME,
-	@end_date DATETIME,
-	@table_name varchar(12),
-	@record_count INT OUTPUT
-AS
-BEGIN
-    -- SET NOCOUNT ON added to prevent extra result sets from
-    -- interfering with SELECT statements.
-    SET NOCOUNT ON;
-	IF OBJECT_ID(@table_name) IS NOT NULL
-    	EXEC ('DROP Table ' + @table_name)
-
---    EXEC('select top(1000) * into ' + @table_name + ' from HourlyOEEValues where Date_time_stamp BETWEEN ' + @start_date + ' and ' + @end_date )
- --   EXEC('select * from HourlyOEEValues where Date_time_stamp BETWEEN ''' + CONVERT(VARCHAR(10),@start_date, 101) + ''' and ''' + CONVERT(VARCHAR(10),@end_date, 101) + '''')
---	select * from HourlyOEEValues where Date_time_stamp BETWEEN @start_date and @end_date
-	declare @sql nvarchar(4000)
-
-select @sql = N'SELECT * into ' + quotename(@table_name) + N' from myhourlyoeevalues  WHERE Date_time_stamp BETWEEN cast(floor(cast(@fromDate as float)) as datetime) AND cast(floor(cast(@toDate as float)) as datetime)'
-
-EXEC sp_executesql @sql, N'@fromDate datetime, @toDate datetime', @start_date, @end_date
-    
-SELECT @record_count = @@ROWCOUNT;
---EXEC('select * from ' + @report_name)
-   
-END;
-select count(*) cnt from MyHourlyOEEValues mho 
-DECLARE	@return_value int,
-@start_date DATETIME,
-@end_date DATETIME,
-@table_name varchar(12),
-@record_count int;
---2004-05-23T14:25:10
---YYYYMMDD or YYYY-MM-DD
---YYYY-MM-DDThh:mm:ss.nnn
-set @start_date ='2019-12-09T00:00:00';
---select @start_date
-set @end_date ='2020-02-15T23:59:59';
---HH:MM:SS.SSS
-set @table_name = 'rpt02080';
-EXEC	@return_value = [dbo].[SprocTest] @start_date,@end_date,@table_name,@record_count OUTPUT
-select '@record_count' = @record_count
-SELECT	'Return Value' = @return_value
-
-GO
---drop table rpt02080
-select top(10) * from rpt02080 order by id
-
-DECLARE	@return_value int,
-@start_date DATETIME,
-@end_date DATETIME,
-@table_name varchar(12),
-@record_count int;
---2004-05-23T14:25:10
-set @start_date ='2020-02-09T00:00:00';
-set @end_date ='2020-02-15T23:59:59';
-select 
---top(1000) *
-count(*) cnt 
-from HourlyOEEValues where Date_time_stamp BETWEEN @start_date and @end_date
-
-
-set @table_name = 'rpt02080'
---Select “Valid Years are: ” & DatePart(“yyyy”, orderDate) WHERE DatePart(“yyyy”,orderDate) > 2001
-
-SELECT 
-m,d,h
-from
-(
-	SELECT 
-	--distinct Data_hour 
-	DATEPART(month, Date_time_stamp ) m,
-	DATEPART(day, Date_time_stamp ) d, 
-	DATEPART(hour, Date_time_stamp ) h 
-	FROM HourlyOEEValues 
-	where Date_time_stamp BETWEEN @start_date and @end_date
-)s1
-group by m,d,h
-
-GROUP BY DATEPART(month, Date_time_stamp ),DATEPART(day, Date_time_stamp ), DATEPART(hour, Date_time_stamp )
-having Date_time_stamp BETWEEN @start_date and @end_date
-order by DATEPART(month, Date_time_stamp ),DATEPART(day, Date_time_stamp ), DATEPART(hour, Date_time_stamp ) 
-
 
 SELECT 
 --distinct Data_hour 
@@ -167,20 +38,221 @@ having Date_time_stamp BETWEEN @start_date and @end_date
 order by Data_hour 
 ORDER BY id 
 OFFSET 20 ROWS FETCH NEXT 10 ROWS ONLY;
-/*
 
-DECLARE @sql nvarchar(4000)
+select count(*) cnt from MyHourlyOEEValues mho 
+DECLARE	@return_value int,
+@start_date DATETIME,
+@end_date DATETIME,
+@table_name varchar(12),
+@record_count int;
+--2004-05-23T14:25:10
+--YYYYMMDD or YYYY-MM-DD
+--YYYY-MM-DDThh:mm:ss.nnn
+set @start_date ='2020-02-09T00:00:00';
+--select @start_date
+set @end_date ='2020-02-15T23:59:59';
+--HH:MM:SS.SSS
+set @table_name = 'rpt02080';
+EXEC	@return_value = [dbo].[Sproc200206] @start_date,@end_date,@table_name,@record_count OUTPUT
+select @record_count 
 
-select @sql = N'
-  SELECT B.FacId 
-       , B.FacName
-       , B.BookCode
-       , B.BookName
-       , B.Quantity
-       , CONVERT(VARCHAR(10), B.TillDate, 104) AS TILLDATE 
-    FROM ' + quotename(@TABLE) + N' B
-   WHERE B.TillDate BETWEEN cast(floor(cast(@fromDate as float)) as datetime)
-                        AND cast(floor(cast(@toDate as float)) as datetime)'
+GO
+select * from rpt02080 order by primary_key
+--drop table rpt02080
+select top(10) * from rpt02080 order by id
 
-EXEC sp_executesql @sql, N'@fromDate datetime, @toDate datetime', @FROMDATE, @TODATE
+--drop PROCEDURE  sproc200206
+CREATE PROCEDURE Sproc200206
+	@start_date DATETIME,
+	@end_date DATETIME,
+	@table_name varchar(12),
+	@record_count INT OUTPUT
+AS
+BEGIN
+-- SET NOCOUNT ON added to prevent extra result sets from
+-- interfering with SELECT statements.
+SET NOCOUNT ON;
+IF OBJECT_ID(@table_name) IS NOT NULL
+	EXEC ('DROP Table ' + @table_name)
+
+/* TESTING ONLY
+DECLARE @start_date DATETIME,
+	@end_date DATETIME,
+	@table_name varchar(12),
+	@record_count INT
+set @start_date ='2020-02-09T00:00:00';
+set @end_date ='2020-02-15T23:59:59';
+set @table_name = 'rpt0213test'
+*/ -- END TESTING ONLY
+	
+Declare @start_year char(4)
+Declare @start_week int
+Declare @end_year char(4)
+Declare @end_week int
+Declare @start_of_week_for_start_date datetime
+Declare @end_of_week_for_end_date datetime
+
+set @start_year = DATEPART(YEAR,@Start_Date)
+set @start_week = DATEPART(WEEK,@Start_Date)
+set @end_year = DATEPART(YEAR,@End_Date)
+set @end_week = DATEPART(WEEK,@End_Date)
+
+
+set @start_of_week_for_start_date = DATEADD(wk, DATEDIFF(wk, 6, '1/1/' + @start_year) + (@start_week-1), 6)  --start of week
+set @end_of_week_for_end_date = DATEADD(wk, DATEDIFF(wk, 5, '1/1/' + @end_year) + (@end_week-1), 5)  --end of week
+
+set @end_of_week_for_end_date = DATEADD(day, 1, @end_of_week_for_end_date);
+set @end_of_week_for_end_date = DATEADD(second,-1,@end_of_week_for_end_date);
+
+/* may be necessary if multiple calls are done on the same connection
+declare @sqlDropPK nvarchar(4000)
+declare @PKTable nvarchar(50)
+set @PKTable = quotename(@table_name + 'PK')
+--select @PKTable
+set @sqlDropPK = N'DROP Table ' + @PKTable 
+--select @sqlDropPK
+IF OBJECT_ID(@PKTable) IS NOT NULL
+EXEC sp_executesql @sqlDropPK
 */
+--drop table #primary_key
+create table #primary_key
+(
+  primary_key int,
+  year_week int,
+  start_week datetime,
+  end_week datetime,
+  part_number varchar(60),
+  workcenter_code varchar(50)
+)
+insert into #primary_key(primary_key,year_week,start_week,end_week,part_number,workcenter_code)
+(
+  select 
+  --top 10
+  ROW_NUMBER() OVER (
+    ORDER BY year * 100 + week,part_number,workcenter_code
+  ) primary_key,
+  year * 100 + week year_week,
+  DATEADD(wk, DATEDIFF(wk, 6, '1/1/' + CONVERT(varchar, year)) + (week-1), 6) start_week, 
+  DATEADD(wk, DATEDIFF(wk, 5, '1/1/' + CONVERT(varchar, year)) + (week-1), 5) end_week, 
+  part_number,
+  workcenter_code
+  from 
+  (
+    select
+    DATEPART(YEAR,date_time_stamp) year,
+    DATEPART(WEEK,date_time_stamp) week,
+    part_number,
+    workcenter_code
+    from HourlyOEEValues 
+    where date_time_stamp between @start_of_week_for_start_date and @end_of_week_for_end_date
+  )s1 
+  group by year,week,part_number,workcenter_code
+)  
+
+--drop table #primary_key
+--select count(*) #primary_key from #primary_key  --16
+--select top(100) * from #primary_key
+--FORMAT ( @d, 'd', 'en-US' ) 
+create table #set2group
+(
+	primary_key int,
+	Hourly_planned_production_count int,
+	Hourly_actual_production_count int,
+	scrap_count int,
+	Downtime_minutes float
+)
+
+insert into #set2group (primary_key,Hourly_planned_production_count,Hourly_actual_production_count,scrap_count,Downtime_minutes)
+(
+select
+pk.primary_key, 
+hv.Hourly_planned_production_count,
+hv.Hourly_actual_production_count,
+hv.scrap_count,
+hv.Downtime_minutes
+from #primary_key pk
+inner join
+(
+  select
+  DATEPART(YEAR,date_time_stamp) * 100 + DATEPART(WEEK,date_time_stamp) year_week,
+    part_number,
+    workcenter_code,
+	Hourly_planned_production_count,
+	Hourly_actual_production_count,
+	scrap_count,
+	Downtime_minutes
+  from HourlyOEEValues 
+  where date_time_stamp between @start_of_week_for_start_date and @end_of_week_for_end_date
+) hv
+on pk.year_week=hv.year_week
+and pk.part_number=hv.Part_number 
+and pk.workcenter_code=hv.Workcenter_Code 
+)
+--select top(100) * from #set2group 
+--select count(*) #set2group from #set2group  --1404
+--drop table #primary_key
+--drop table #set2group
+create table #results
+(
+  primary_key int,
+  start_week varchar(12),
+  end_week varchar(12),
+  part_number varchar(60),
+  workcenter_code varchar(50),
+  planned_production_count int,
+  actual_production_count int,
+  actual_vrs_planned_percent decimal(18,2),
+  scrap_count int,
+  downtime_minutes int
+)
+
+insert into #results (primary_key,start_week,end_week,part_number,workcenter_code,planned_production_count,actual_production_count,actual_vrs_planned_percent,scrap_count,downtime_minutes)
+(
+select
+pk.primary_key,
+CONVERT(VARCHAR, start_week ,101),
+CONVERT(VARCHAR, end_week ,101),
+--FORMAT ( pk.start_week, 'd', 'en-US' ) start_week, 
+--FORMAT ( pk.end_week, 'd', 'en-US' ) end_week, 
+pk.part_number,
+pk.workcenter_code,
+planned_production_count,
+actual_production_count,
+case
+when planned_production_count = 0 then cast(0.00 as decimal(18,2))
+else cast (actual_production_count*100./planned_production_count as decimal(18,2))
+end actual_vrs_planned_percent, 
+scrap_count,
+downtime_minutes 
+from
+(
+select
+primary_key,
+sum(Hourly_planned_production_count) planned_production_count,
+sum(Hourly_actual_production_count) actual_production_count,
+sum(scrap_count) scrap_count,
+floor(sum(Downtime_minutes)) Downtime_minutes 
+from #set2group 
+group by primary_key
+) sg
+inner join #primary_key pk
+on sg.primary_key = pk.primary_key
+)
+--select * from #results 
+--DECLARE @table_name varchar(12),
+--	@record_count INT
+--set @table_name = 'rpt0213test'
+--*/ -- END TESTING ONLY
+--drop table rpt0213test
+declare @sql nvarchar(4000)
+select @sql = N'SELECT * into ' + quotename(@table_name) + N' from #results order by primary_key'
+
+--select @sql = N'SELECT start_week,end_week,part_number,workcenter_code,planned_production_count,actual_production_count,actual_vrs_planned_percent,scrap_count,downtime_minutes into ' + quotename(@table_name) + N' from #results order by primary_key'
+
+EXEC sp_executesql @sql
+    
+SELECT @record_count = @@ROWCOUNT;
+--select @record_count
+END; 
+select * from rpt0213test
+
