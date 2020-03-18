f3acb43bda2623fc4678f9215f23cf1e05bdf954
Brent Groves <brent.groves@gmail.com>
2020-03-18 15:34:08 -0400

    Added/Update source code

--- MRO/sales_release_week_tooling_cost_m2m.sql
+++ MRO/sales_release_week_tooling_cost_m2m.sql
@@ -10,53 +10,104 @@ CREATE TABLE m2mdata01.dbo.ToolingTransLog (
 	UserNumber nvarchar(32),
 	UserName nvarchar(50),
 	Plant nvarchar(3)
-)
+) GO
 */
---select count(*) from dbo.ToolingTransLog --404541
-	Declare @startDateParam DATETIME
-	Declare @endDateParam DATETIME
-	set @startDateParam = DATEADD (month ,-1, GETDATE())
-	set @endDateParam = GETDATE()
+Declare @Start_Date datetime
+Declare @End_Date datetime
+Declare @start_year char(4)
+Declare @start_week int
+Declare @end_year char(4)
+Declare @end_week int
+Declare @start_of_week_for_start_date datetime
+Declare @end_of_week_for_end_date datetime
 
-	IF Object_ID('tempdb..#primary_key') is not null
-	DROP TABLE #primary_key
+set @Start_Date = '20200216'
+set @End_Date = '20200314'
+set @start_year = DATEPART(YEAR,@Start_Date)
+set @start_week = DATEPART(WEEK,@Start_Date)
+set @end_year = DATEPART(YEAR,@End_Date)
+set @end_week = DATEPART(WEEK,@End_Date)
+--select  DATEADD(second,-1,DATEADD(day, 1,datefromparts(DATEPART(YEAR,@End_Date), 12, 31)))
+--select convert(datetime,DATEADD(day, 1,datefromparts(DATEPART(YEAR,@End_Date), 12, 31)))
+--set @end_of_week_for_end_date = DATEADD(second,-1,convert(datetime,DATEADD(day, 1,datefromparts(DATEPART(YEAR,@End_Date), 12, 31))))  
+--select cast(DATEPART(YEAR,@Start_Date) as nvarchar)
+--select cast(cast(DATEPART(YEAR,@Start_Date) as nvarchar)+'-01-01' as datetime)
+--select convert(datetime, @dt, 101)
+if DATEPART(WEEK,@Start_Date) = 1
+set @start_of_week_for_start_date = cast(cast(DATEPART(YEAR,@Start_Date) as nvarchar)+'-01-01' as datetime)
+else
+set @start_of_week_for_start_date = DATEADD(wk, DATEDIFF(wk, 6, '1/1/' + @start_year) + (@start_week-1), 6)  --start of week
+--select DATEPART(WEEK,@End_Date)
+--select DATEPART(MONTH,DATEADD(wk, DATEDIFF(wk, 5, '1/1/' + CONVERT(varchar, DATEPART(YEAR,@End_Date))) + (DATEPART(WEEK,@End_Date)-1), 5))
+if DATEPART(WEEK,@End_Date) > 51 and  (  DATEPART(MONTH,DATEADD(wk, DATEDIFF(wk, 5, '1/1/' + CONVERT(varchar, DATEPART(YEAR,@End_Date))) + (DATEPART(WEEK,@End_Date)-1), 5))   =1)
+set @end_of_week_for_end_date = DATEADD(second,-1,convert(datetime,DATEADD(day, 1,cast(cast(DATEPART(YEAR,@Start_Date) as nvarchar)+'-12-31' as datetime))))
+else
+set @end_of_week_for_end_date = DATEADD(second,-1,DATEADD(day,1,DATEADD(wk, DATEDIFF(wk, 5, '1/1/' + @end_year) + (@end_week-1), 5)))  --end of week
 
-	/*
-	primary_key: Determine primary key of result set.
-	*/
-	create table #primary_key
-	(
-	  	primary_key int identity(1,1),
-	--  	plant nvarchar(3),
-		PartNumber nvarchar(25),
-	)
+--select @start_of_week_for_start_date,@end_of_week_for_end_date
+/*
+primary_key: Determine primary key of result set.
+  ROW_NUMBER() OVER (
+    ORDER BY year_week
+  ) primary_key,
 
-	insert into #primary_key (PartNumber)
-	(
-		select 
-		DISTINCT PartNumber 
-		from toolingtranslog
-		where transtartdatetime >= @startDateParam
-		and transtartdatetime <= @endDateParam
-		and PartNumber <> ''
-	)
-
-	--select count(*) #primary_key from #primary_key  --217
-
-	IF Object_ID('tempdb..#set2group') is not null
-	drop table #set2group 
+**/
 	
-	create table #set2group
-	(
-		PartNumber nvarchar(25),
-		Qty int,
-		UnitCost decimal(18,2),
-		TotalCost decimal(18,2)
-	)
+IF Object_ID('tempdb..#primary_key') is not null
+DROP TABLE #primary_key
 
-	insert into #set2group (PartNumber,Qty,UnitCost,TotalCost)
-	(
+create table #primary_key
+(
+  primary_key int,
+  year_week int,
+  year_week_fmt varchar(20),
+  start_week datetime,
+  end_week datetime,
+  part_no nvarchar(25)
+)
+
+insert into #primary_key(primary_key,year_week,year_week_fmt,start_week,end_week,part_no)
+(
+   select 
+  ROW_NUMBER() OVER (
+    ORDER BY year_week,part_no
+  ) primary_key,
+  year_week, 
+  year_week_fmt,
+  start_week,
+  end_week,
+  part_no
+  from 
+  (
+    select
+    DATEPART(YEAR,tl.transtartdatetime) * 100 + DATEPART(WEEK,tl.transtartdatetime) year_week,
+    case     
+    when DATEPART(WEEK,tl.transtartdatetime) < 10 then convert(varchar,DATEPART(YEAR ,tl.transtartdatetime)) + '-0' + convert(varchar,DATEPART(WEEK,tl.transtartdatetime))
+    else 
+     convert(varchar,DATEPART(YEAR ,tl.transtartdatetime)) + '-' + convert(varchar,DATEPART(WEEK,tl.transtartdatetime))
+    end year_week_fmt,
+    case 
+    when DATEPART(WEEK,tl.transtartdatetime) = 1 then cast(cast(DATEPART(YEAR,tl.transtartdatetime) as nvarchar)+'-01-01' as datetime)
+    else DATEADD(wk, DATEDIFF(wk, 6, '1/1/' + CONVERT(varchar, DATEPART(YEAR,tl.transtartdatetime))) + (DATEPART(WEEK,tl.transtartdatetime)-1), 6) 
+    end start_week, 
+    case                                                        
+    when DATEPART(WEEK,tl.transtartdatetime) > 51 and  (DATEPART(MONTH,DATEADD(wk, DATEDIFF(wk, 5, '1/1/' + CONVERT(varchar, DATEPART(YEAR,tl.transtartdatetime))) + (DATEPART(WEEK,tl.transtartdatetime)-1), 5))=1)  then DATEADD(second,-1,convert(datetime,DATEADD(day, 1,cast(cast(DATEPART(YEAR,tl.transtartdatetime) as nvarchar)+'-12-31' as datetime))))
+    else DATEADD(second,-1,DATEADD(day, 1,DATEADD(wk, DATEDIFF(wk, 5, '1/1/' + CONVERT(varchar, DATEPART(YEAR,tl.transtartdatetime))) + (DATEPART(WEEK,tl.transtartdatetime)-1), 5)))
+    end end_week,
+    tl.PartNumber part_no
+	from toolingtranslog tl
+	where tl.transtartdatetime between @start_of_week_for_start_date and @end_of_week_for_end_date
+    and tl.PartNumber <> ''
+    --where sr.ship_date between @start_of_week_for_start_date and @end_of_week_for_end_date
+    --and pl.part_key not in (select * from #filter)
+  )s1 
+  group by s1.year_week,s1.year_week_fmt,s1.start_week,s1.end_week,s1.part_no  -- sales release to shipper_line is a 1 to many relationship so make sure these records are distinct
+
+)
+--select * from #primary_key
+/*
 		select 
+		TranStartDateTime,
 		PartNumber,
 		Qty,
 		UnitCost,
@@ -65,47 +116,109 @@ CREATE TABLE m2mdata01.dbo.ToolingTransLog (
 		where transtartdatetime >= @startDateParam
 		and transtartdatetime <= @endDateParam
 		and PartNumber <> ''
-	)
+		order by transtartdatetime desc
+*/
 	
-	--select count(*) #set2group from #set2group  --7053
-	--select top(50) *  from #set2group
+	
+	--select count(*) #primary_key from #primary_key  --217
 
-	IF Object_ID('tempdb..#sales_release_week_tooling_cost_m2m') is not null
-	drop table #sales_release_week_tooling_cost_m2m 
+IF Object_ID('tempdb..#set2group') is not null
+drop table #set2group 
 	
-	create table #sales_release_week_tooling_cost_m2m
-	(
-		PartNumber nvarchar(25),
-		TotalQty int,
-		TotalCost decimal(18,2)
-	)
-	
-	insert into #sales_release_week_tooling_cost_m2m (PartNumber,TotalQty,TotalCost)
+create table #set2group
+(
+  	primary_key int,
+	part_no nvarchar(25),
+	total_cost decimal(18,2)
+)
+
+insert into #set2group (primary_key,part_no,total_cost)
+(
+	select 
+	pk.primary_key,
+	s1.part_no,
+	s1.total_cost
+	from 
 	(
 		select 
-		pk.PartNumber, 
-		case 
-		when stg.qty is null then 0 
-		else stg.qty 
-		end TotalQty,
-		case 
-		when stg.TotalCost is null then 0.00 
-		else stg.TotalCost 
-		end TotalCost
-		from #primary_key pk 
-		inner join 
-		(
-			select 
-			stg.PartNumber,
-			sum(stg.Qty) Qty,
-			sum(stg.TotalCost) TotalCost
-			from #set2group stg
-			group by stg.PartNumber 
-		) stg
-		on pk.PartNumber=stg.PartNumber
-	)
+    	DATEPART(YEAR,tl.transtartdatetime) * 100 + DATEPART(WEEK,tl.transtartdatetime) year_week,
+		PartNumber part_no,
+		Cast(Qty*UnitCost as decimal(18,2)) as total_cost
+		from toolingtranslog tl
+		where tl.transtartdatetime between @start_of_week_for_start_date and @end_of_week_for_end_date
+		and PartNumber <> ''
+	)s1 
+	inner join #primary_key pk 
+	on s1.year_week=pk.year_week
+	and s1.part_no=pk.part_no 
+)
+	
+	
+--select count(*) #set2group from #set2group  --7053
+--select top(50) *  from #set2group 
+
+IF Object_ID('tempdb..#sales_release_week_tooling_cost_m2m') is not null
+drop table #sales_release_week_tooling_cost_m2m 
+
+/*
+Final set: Join of all intermediate sets.
+*/
+
+create table #sales_release_week_tooling_cost_m2m
+(
+  	primary_key int,
+  	year_week int,
+  	year_week_fmt varchar(20),
+  	start_week datetime,
+  	end_week datetime,
+  	part_no varchar (25),
+	total_cost decimal(18,2)
+)
+
+
+insert into #sales_release_week_tooling_cost_m2m (primary_key,year_week,year_week_fmt,start_week,end_week,part_no,total_cost)
+(
+  select
+  primary_key,
+  year_week,
+  year_week_fmt,
+  start_week,
+  end_week,
+  part_no,
+  total_cost
+  from
+  (
+    select 
+    pk.primary_key,
+    pk.year_week,
+    pk.year_week_fmt,
+    pk.start_week,
+    pk.end_week,
+    pk.part_no,
+	case 
+	when stg.total_cost is null then 0.00 
+	else stg.total_cost 
+	end total_cost
+    from #primary_key pk
+	inner join 
+	(
+		select
+		stg.primary_key,
+		sum(stg.total_cost) total_cost
+		from #set2group stg
+		group by stg.primary_key 
+	) stg
+	on pk.primary_key=stg.primary_key
+  )s1
+  
+)
+
 select * 
 from #sales_release_week_tooling_cost_m2m
-order by TotalCost desc
+order by part_no desc
+
+select top(100) * 
+from dbo.ToolingTransLog tl 
+order by TranStartDateTime desc 
 
 
--- MRO/sales_release_week_volume_revenue_shipped_cm.sql
+++ MRO/sales_release_week_volume_revenue_shipped_cm.sql
@@ -1,3 +1,4 @@
+--drop table bt_sales_release_week_volume_revenue
 create table bt_sales_release_week_volume_revenue
 (
   primary_key int,
@@ -26,13 +27,22 @@ top 1000 *
 from bt_sales_release_week_volume_revenue --0
 
 
+
+
+/*
+Final set: Join of all intermediate sets.
+*/
+--drop table bt_sales_release_week_tooling_cost_m2m
 create table bt_sales_release_week_tooling_cost_m2m
 (
-	PartNumber nvarchar(25),
-	TotalQty int,
-	TotalCost decimal(18,2)
+  	primary_key int,
+  	year_week int,
+  	year_week_fmt varchar(20),
+  	start_week datetime,
+  	end_week datetime,
+  	part_no varchar (25),
+	total_cost decimal(18,2)
 )
-
 Bulk insert bt_sales_release_week_tooling_cost_m2m
 from 'c:\ToolingCost.csv'
 with
@@ -44,4 +54,83 @@ with
 select
 --count(*)
 top 1000 * 
+from bt_sales_release_week_tooling_cost_m2m --213
+
+
+select 
+distinct year_week
+from bt_sales_release_week_volume_revenue 
+order by year_week desc 
+
+
+select
+--count(*)
+--top 1000 * 
+DISTINCT tran
 from bt_sales_release_week_tooling_cost_m2m --0
+
+select
+--count(*)
+vr.year_week,
+vr.part_no,
+tc.year_week,
+tc.part_no, 
+'USD' as Currency,
+vr.year_week as Period,
+vr.volume as [Actual Volume],
+vr.revenue as [Actual Revenue (Local)],
+vr.revenue as [Actual Revenue (USD)],
+tc.total_cost [Actual Material Cost (Local)],
+tc.total_cost [Actual Material Cost (USD)]
+from bt_sales_release_week_volume_revenue vr 
+full join bt_sales_release_week_tooling_cost_m2m tc 
+on vr.part_no = tc.part_no 
+and vr.year_week = tc.year_week
+--where vr.part_no is null
+--and tc.PartNumber !=''
+
+select
+--count(*)
+distinct tc.part_no,tc.year_week 
+from bt_sales_release_week_volume_revenue vr 
+full join bt_sales_release_week_tooling_cost_m2m tc 
+on vr.part_no = tc.part_no 
+where vr.part_no is null
+order by tc.part_no,tc.year_week asc
+
+select 
+tc.part_no tc_part_no,
+vr.part_no vr_part_no
+from 
+(
+	select
+	--count(*)
+	distinct tc.part_no
+	from bt_sales_release_week_tooling_cost_m2m tc 
+)tc 
+full join 
+(
+	select
+	--count(*)
+	distinct vr.part_no
+	from bt_sales_release_week_volume_revenue vr 
+)vr 
+on tc.part_no=vr.part_no 
+
+
+
+
+order by tc.part_no
+
+/*
+select 
+vr.part_no,
+'USD' as Currency,
+vr.year_week as Period,
+vr.volume as [Actual Volume],
+vr.revenue as [Actual Revenue (Local)],
+vr.revenue as [Actual Revenue (USD)]
+from #sales_release_week_volume_revenue vr
+order by vr.revenue desc
+*/
+
