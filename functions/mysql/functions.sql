f1cf500bf627a9bf101c8b11120e998dec0f9b89
Brent Groves <brent.groves@gmail.com>
2020-05-20 11:51:09 -0400

    Added/Update source code

--- functions/mysql/functions.sql
+++ functions/mysql/functions.sql
@@ -1,73 +1,121 @@
 /*
  * Find first day of WEEK
  */
+set @DEBUG = true;
+TRUNCATE TABLE debugger; 
+-- set @day = STR_TO_DATE('01/04/2020 23:59:59','%m/%d/%Y %H:%i:%s'); -- week 0
+-- set @day = STR_TO_DATE('01/05/2020 23:59:59','%m/%d/%Y %H:%i:%s'); -- week 1
+set @day =STR_TO_DATE('12/31/2019 23:59:59','%m/%d/%Y %H:%i:%s'); -- week 52
+SELECT FIRST_DAY_OF_WEEK(@day);
+SELECT * from debugger;
 
-
-	-- set @startDate = STR_TO_DATE('02/09/2020 23:59:59','%m/%d/%Y %H:%i:%s');
-	-- set @startDate = STR_TO_DATE('01/01/2020 23:59:59','%m/%d/%Y %H:%i:%s'); -- week 0
-	set @startDate = STR_TO_DATE('01/04/2020 23:59:59','%m/%d/%Y %H:%i:%s'); -- week 0
-	-- set @startDate = STR_TO_DATE('01/05/2020 23:59:59','%m/%d/%Y %H:%i:%s'); -- week 1
-	-- set @startDate =STR_TO_DATE('12/31/2019 23:59:59','%m/%d/%Y %H:%i:%s'); -- week 52
-	-- SELECT @startDate;
-	SELECT FIRST_DAY_OF_WEEK(@startDate);
 drop function FIRST_DAY_OF_WEEK;
-CREATE FUNCTION FIRST_DAY_OF_WEEK(day DATETIME)
+CREATE FUNCTION FIRST_DAY_OF_WEEK(pDay DATETIME)
 RETURNS DATETIME DETERMINISTIC
 BEGIN
-
-	set @day = day;
-	set @week = week(@day);
-	if @week = 0 then
-	 	set @year = year(@day);
-		set @dayOne = concat('01/01/',@year,' 00:00:00');
-		set @firstDay = STR_TO_DATE(@dayOne,'%m/%d/%Y %H:%i:%s');
+	DECLARE day,firstDay,floorDate DATETIME;
+	DECLARE week,year int;
+	DECLARE dayOne char(20);
+	set day = pDay;
+	set week = week(day);
+	if week = 0 then
+	 	set year = year(day);
+		set dayOne = concat('01/01/',year,' 00:00:00');
+		set firstDay = STR_TO_DATE(dayOne,'%m/%d/%Y %H:%i:%s');
 	else
-		set @floorDate = ADDDATE(DATE(@day),INTERVAL 0 second);
-		set @firstDay = subdate(@floorDate, INTERVAL DAYOFWEEK(@floorDate)-1 DAY);
+		set floorDate = ADDDATE(DATE(day),INTERVAL 0 second);
+		set firstDay = subdate(floorDate, INTERVAL DAYOFWEEK(floorDate)-1 DAY);
 	end if;
-	return @firstDay;
+	IF @DEBUG then
+		INSERT INTO debugger VALUES (CONCAT('[FIRST_DAY_OF_WEEK: day=',day,',week=',week,',firstDay=',firstDay,']'));
+	End If;
+	return firstDay;
 END;	
 
 -- set @startDate = STR_TO_DATE('01/01/2020 23:59:59','%m/%d/%Y %H:%i:%s'); -- week 0
-SET @msg := '';
+set @DEBUG = true;
+TRUNCATE TABLE debugger; 
+set @day =STR_TO_DATE('03/1/2020 23:59:59','%m/%d/%Y %H:%i:%s'); -- week 52
+-- set @day =STR_TO_DATE('03/14/2019 23:59:59','%m/%d/%Y %H:%i:%s'); -- week 52
+-- set @day = STR_TO_DATE('01/04/2020 23:59:59','%m/%d/%Y %H:%i:%s'); -- week 0
+-- set @day = STR_TO_DATE('01/05/2020 23:59:59','%m/%d/%Y %H:%i:%s'); -- week 1
+-- set @day =STR_TO_DATE('12/31/2019 23:59:59','%m/%d/%Y %H:%i:%s'); -- week 52
+-- set @day = STR_TO_DATE('02/09/2020 00:00:00','%m/%d/%Y %H:%i:%s'); -- week 0
+
+SELECT LAST_DAY_OF_WEEK(@day);
+SELECT * from debugger;
+
+CREATE TABLE debugger
+( debug_comment CHAR(255)) ENGINE=MEMORY;
+
+drop function LAST_DAY_OF_WEEK;
+CREATE FUNCTION LAST_DAY_OF_WEEK(pDay DATETIME)
+RETURNS DATETIME DETERMINISTIC
+BEGIN
+	DECLARE day,lastDay,nextDay,floorDate DATETIME;
+	DECLARE week,year,endWeek int;
+	set day = pDay;
+	set week = week(day);
+	set floorDate = ADDDATE(DATE(day),INTERVAL 0 second);
+	set lastDay = ADDDATE(floorDate,INTERVAL 7 - DAYOFWEEK(floorDate) DAY );
+	set nextDay = ADDDATE(DATE(lastDay),INTERVAL 1 DAY);
+	set lastDay = subdate(nextDay, INTERVAL 1 second);
+	set endWeek = week(lastDay);
+	-- make the last day = 12/31 if endWeek goes into the next year. 
+	if week > endWeek then
+	 	set year = year(day);
+	 	set lastDay = STR_TO_DATE(concat('12/31/',year,' 23:59:59'),'%m/%d/%Y %H:%i:%s');
+	end if;
+	IF @DEBUG then
+		INSERT INTO debugger VALUES (CONCAT('[LAST_DAY_OF_WEEK: day=',day,',week=',week,',floorDate=',floorDate,',lastDay=',lastDay,',nextDay=',nextDay,',endWeek=',endWeek,']'));
+	End If;
+	return lastDay;
+END;
+
+set @DEBUG = true;
+TRUNCATE TABLE debugger; 
 -- set @day = STR_TO_DATE('01/04/2020 23:59:59','%m/%d/%Y %H:%i:%s'); -- week 0
 -- set @day = STR_TO_DATE('01/05/2020 23:59:59','%m/%d/%Y %H:%i:%s'); -- week 1
 set @day =STR_TO_DATE('12/31/2019 23:59:59','%m/%d/%Y %H:%i:%s'); -- week 52
 SELECT LAST_DAY_OF_WEEK(@day);
-SELECT @msg;
+SELECT * from debugger;
 
-drop function LAST_DAY_OF_WEEK;
-CREATE FUNCTION LAST_DAY_OF_WEEK(day DATETIME)
-RETURNS DATETIME DETERMINISTIC
+CREATE TABLE debugger
+( debug_comment CHAR(255)) ENGINE=MEMORY;
+
+
+set @DEBUG = true;
+TRUNCATE TABLE debugger; 
+-- set @day = STR_TO_DATE('12/31/2019 23:59:59','%m/%d/%Y %H:%i:%s');
+-- set @day = STR_TO_DATE('01/04/2020 23:59:59','%m/%d/%Y %H:%i:%s'); -- week 0
+set @day = STR_TO_DATE('01/05/2020 23:59:59','%m/%d/%Y %H:%i:%s'); -- week 1
+SELECT STD_WEEK(@day);
+SELECT * from debugger;
+
+/*
+-- THIS IS NEEDED TO MAKE SURE MSSQL WEEKS ARE THE SAME AS MYSQL DATES
+-- MySQL datetime formats 'YYYY-MM-DD hh:mm:ss'
+*/
+drop function STD_WEEK;
+CREATE FUNCTION STD_WEEK(pDay DATETIME)
+RETURNS INT DETERMINISTIC
 BEGIN
-	set @day = day;
-	set @week = week(@day);
-	set @floorDate = ADDDATE(DATE(@day),INTERVAL 0 second);
-	set @lastDay = ADDDATE(@floorDate,INTERVAL 7 - DAYOFWEEK(@floorDate) DAY );
-	set @nextDay = ADDDATE(DATE(@lastDay),INTERVAL 1 DAY);
-	set @endWeek = week(subdate(@nextDay, INTERVAL 1 second));
--- set @lastDay = STR_TO_DATE(concat('12/31/',@year,' 00:00:00'),'%m/%d/%Y %H:%i:%s');
--- SELECT CONCAT('[Debug #',@week,',',@endWeek,']') INTO @msg;
--- if the week of @day > 51 and the end of week for @day is in week 0 then 
-	-- make the last day = 12/13 
-	if @week > @endWeek then
-	 	set @year = year(@day);
-	 	set @lastDay = STR_TO_DATE(concat('12/31/',@year,' 00:00:00'),'%m/%d/%Y %H:%i:%s');
-  		SELECT CONCAT('[Debug # @week > @endWeek]') INTO @msg;
-	else
-  		SELECT CONCAT('[Debug # @week <= @endWeek]') INTO @msg;
-	 	set @lastDay = subdate(@nextDay, INTERVAL 1 second);
-	end if;
-	return @lastDay;
+	DECLARE day,yearJan1 DATETIME;
+	DECLARE week,year,yearJan1DOW int;
+	set day = pDay;
+	set week = WEEK(day);
+	set year = YEAR(day);
+
+-- THIS IS NEEDED TO MAKE SURE MSSQL WEEKS ARE THE SAME AS MYSQL DATES
+	-- MySQL datetime formats 'YYYY-MM-DD hh:mm:ss'
+	set yearJan1 = STR_TO_DATE(concat('01/01/',year,' 00:00:00'),'%m/%d/%Y %H:%i:%s');
+	set yearJan1DOW = DAYOFWEEK(yearJan1);
+
+	set week = if(yearJan1DOW = 1,week,week + 1);
+	IF @DEBUG then
+		INSERT INTO debugger VALUES (CONCAT('[STD_WEEK:day=',day,',week=',week,',year=',year,',yearJan1=',yearJan1,',yearJan1DOW=',yearJan1DOW,']'));
+	End If;
+
+	return week;
 END;
-	set @day =STR_TO_DATE('12/31/2019 23:59:59','%m/%d/%Y %H:%i:%s'); -- week 52
-	set @week = week(@day);
-	set @floorDate = ADDDATE(DATE(@day),INTERVAL 0 second);
-	set @lastDay = ADDDATE(@floorDate,INTERVAL 7 - DAYOFWEEK(@floorDate) DAY );
-	set @nextDay = ADDDATE(DATE(@lastDay),INTERVAL 1 DAY);
-	select week(subdate(@nextDay, INTERVAL 1 second));
-
-
-
-
   
--- kors/MySQL/Sproc200206.sql
+++ kors/MySQL/Sproc200206.sql
@@ -27,15 +27,16 @@ CREATE TABLE TempTable (
 -- set @startDate = STR_TO_DATE('02/09/2020 00:00:00','%m/%d/%Y %H:%i:%s'); -- week 0
 -- set @startDate = STR_TO_DATE('02/15/2020 00:00:00','%m/%d/%Y %H:%i:%s'); -- week 0
 -- set @startDate = STR_TO_DATE('03/01/2020 00:00:00','%m/%d/%Y %H:%i:%s'); -- week 0
+set @startDate = STR_TO_DATE('03/29/2020 00:00:00','%m/%d/%Y %H:%i:%s'); -- week 0
 -- set @startDate =STR_TO_DATE('12/31/2019 23:59:59','%m/%d/%Y %H:%i:%s'); -- week 52
 -- set @startDate = STR_TO_DATE('01/04/2020 23:59:59','%m/%d/%Y %H:%i:%s'); -- week 0
-set @startDate = STR_TO_DATE('01/05/2020 23:59:59','%m/%d/%Y %H:%i:%s'); -- week 1
+-- set @startDate = STR_TO_DATE('01/05/2020 23:59:59','%m/%d/%Y %H:%i:%s'); -- week 1
 -- set @endDate = STR_TO_DATE('02/09/2020 00:00:00','%m/%d/%Y %H:%i:%s'); -- week 0
 -- set @endDate = STR_TO_DATE('02/15/2020 00:00:00','%m/%d/%Y %H:%i:%s'); -- week 0
--- set @endDate =STR_TO_DATE('03/14/2019 23:59:59','%m/%d/%Y %H:%i:%s'); -- week 52
+set @endDate = STR_TO_DATE('04/04/2020 23:59:59','%m/%d/%Y %H:%i:%s'); -- week 52
 -- set @endDate =STR_TO_DATE('12/31/2019 23:59:59','%m/%d/%Y %H:%i:%s'); -- week 52
 -- set @endDate = STR_TO_DATE('01/04/2020 23:59:59','%m/%d/%Y %H:%i:%s'); -- week 0
-set @endDate = STR_TO_DATE('01/05/2020 23:59:59','%m/%d/%Y %H:%i:%s'); -- week 1
+-- set @endDate = STR_TO_DATE('01/05/2020 23:59:59','%m/%d/%Y %H:%i:%s'); -- week 1
 set @tableName = 'TempTable';
 set @DEBUG = true;
 TRUNCATE TABLE debugger; 
@@ -53,10 +54,14 @@ CREATE PROCEDURE Sproc200206
 	OUT pRecordCount INT 
 )
 BEGIN
-	SET @table_name = pTableName;
-	set @startDate =pStartDate;
-	set @endDate =pEndDate;
-	SET @tableName = pTableName;  
+	DECLARE startDate,endDate,startWeekStartDate,endWeekEndDate DATETIME;
+	DECLARE tableName varchar(12);
+	DECLARE startWeek,endWeek INT;
+
+	SET tableName = pTableName;
+	set startDate =pStartDate;
+	set endDate =pEndDate;
+	SET tableName = pTableName;  
 -- /* 
     -- https://www.w3resource.com/mysql/date-and-time-functions/mysql-week-function.php
     -- Week 1 is the first week with a sunday; range: 0 - 53
@@ -71,77 +76,167 @@ BEGIN
    	PREPARE stmt1 FROM @sql_query;
 	execute stmt1;
 	
-	set @startWeek = WEEK(@startDate);
-	set @startYear = YEAR(@startDate);
-	set @endYear = YEAR(@endDate);
-	set @endWeek = WEEK(@endDate);
-	-- THIS IS NEEDED TO MAKE SURE MSSQL WEEKS ARE THE SAME AS MYSQL DATES
-	set @startYearJan1 = CONCAT(@startYear,'-01-01T00:00:00');
-	set @startYearJan1DOW = DAYOFWEEK(@startYearJan1);
-	set @endYearJan1 = CONCAT(@endYear,'-01-01T00:00:00');
-	set @endYearJan1DOW = DAYOFWEEK(@endYearJan1);
+	set startWeek = STD_WEEK(startDate);
+	set endWeek = STD_WEEK(endDate);
 
-	set @startWeek = if(@startYearJan1DOW = 1,@startWeek,@startWeek + 1); 
-	set @endWeek = if(@endYearJan1DOW = 1,@endWeek,@endWeek + 1); 
-	set @startWeekStartDate = FIRST_DAY_OF_WEEK(@startDate);
-	set @endWeekEndDate = LAST_DAY_OF_WEEK(@endDate);
+	set startWeekStartDate = FIRST_DAY_OF_WEEK(startDate);
+	set endWeekEndDate = LAST_DAY_OF_WEEK(endDate);
     IF @DEBUG then
-      	INSERT INTO debugger VALUES (CONCAT('[Debug # ',@startYear,',',@startWeek,',',@endYear,',',@endWeek,',',@startWeekStartDate,',',@endWeekEndDate,']'));
+      	INSERT INTO debugger VALUES (CONCAT('[Sproc200206:startDate=',startDate,',endDate=',endDate,',startWeek=',startWeek,',endWeek=',endWeek,',startWeekStartDate=',startWeekStartDate,',endWeekEndDate=',endWeekEndDate,']'));
 	End If;
-	set pRecordCount = 5;
-end
--- START HERE
--- drop table #primary_key
-create table #primary_key
-(
-  primary_key int,
-  year_week int,
-  start_week datetime,
-  end_week datetime,
-  part_number varchar(60),
-  workcenter_code varchar(50)
-)
-insert into #primary_key(primary_key,year_week,start_week,end_week,part_number,workcenter_code)
-(
-  select 
-  --top 10
-  ROW_NUMBER() OVER (
-    ORDER BY year * 100 + week,part_number,workcenter_code
-  ) primary_key,
-  year * 100 + week year_week,
-  DATEADD(wk, DATEDIFF(wk, 6, '1/1/' + CONVERT(varchar, year)) + (week-1), 6) start_week, 
-  DATEADD(wk, DATEDIFF(wk, 5, '1/1/' + CONVERT(varchar, year)) + (week-1), 5) end_week, 
-  part_number,
-  workcenter_code
-  from 
-  (
-    select
-    DATEPART(YEAR,date_time_stamp) year,
-    DATEPART(WEEK,date_time_stamp) week,
-    part_number,
-    workcenter_code
-    from HourlyOEEValues 
-    where date_time_stamp between @start_of_week_for_start_date and @end_of_week_for_end_date
-  )s1 
-  group by year,week,part_number,workcenter_code
-)  
+
+	DROP TABLE IF EXISTS primary_key;
+	create temporary table primary_key
+	(
+	  primary_key int,
+	  year_week int,
+  	  year_week_fmt varchar(10),
+      start_week datetime,
+	  end_week datetime,
+	  part_number varchar(60),
+	  workcenter_code varchar(50)
+	);
+	insert into primary_key(primary_key,year_week,year_week_fmt,start_week,end_week,part_number,workcenter_code)
+	(
+	  select 
+	  ROW_NUMBER() OVER (
+	    ORDER BY year * 100 + week,part_number,workcenter_code
+	  ) primary_key,
+	  year * 100 + week year_week,
+	  CASE 
+	  when week < 10 then CONCAT(year,'-0',week)
+	  else CONCAT(year,'-',week) 
+	  end year_week_fmt,
+	  start_week,
+	  end_week,
+	  part_number,
+	  workcenter_code
+	  from 
+	  (
+	    select
+	    YEAR(date_time_stamp) year,
+	    STD_WEEK(date_time_stamp) week,
+		FIRST_DAY_OF_WEEK(date_time_stamp) start_week,
+	  	LAST_DAY_OF_WEEK(date_time_stamp) end_week,
+	    part_number,
+	    workcenter_code
+	    from HourlyOEEValues 
+	    where date_time_stamp between startWeekStartDate and endWeekEndDate
+	  )s1 
+	  group by year,week,start_week,end_week,part_number,workcenter_code
+	);  
+	-- select * from primary_key;
+	DROP TABLE IF EXISTS set2group;
+	create temporary table set2group
+	(
+		primary_key int,
+		Hourly_planned_production_count int,
+		Hourly_actual_production_count int,
+		scrap_count int,
+		Downtime_minutes float
+	);
+	insert into set2group (primary_key,Hourly_planned_production_count,Hourly_actual_production_count,scrap_count,Downtime_minutes)
+	(
+	select
+	pk.primary_key, 
+	hv.Hourly_planned_production_count,
+	hv.Hourly_actual_production_count,
+	hv.scrap_count,
+	hv.Downtime_minutes
+	from primary_key pk
+	inner join
+	(
+	  select
+	    YEAR(date_time_stamp) * 100 + STD_WEEK(date_time_stamp) year_week,
+	    part_number,
+	    workcenter_code,
+		Hourly_planned_production_count,
+		Hourly_actual_production_count,
+		scrap_count,
+		Downtime_minutes
+	  from HourlyOEEValues 
+	  where date_time_stamp between startWeekStartDate and endWeekEndDate
+	) hv
+	on pk.year_week=hv.year_week
+	and pk.part_number=hv.Part_number 
+	and pk.workcenter_code=hv.Workcenter_Code 
+	);
+	-- select * from set2group limit 100;
+
+	DROP TABLE IF EXISTS results;
+	create temporary table results
+	(
+	  primary_key int,
+	  start_week varchar(12),
+	  end_week varchar(12),
+	  part_number varchar(60),
+	  workcenter_code varchar(50),
+	  planned_production_count nvarchar(10),
+	  actual_production_count nvarchar(10),
+	  actual_vrs_planned_percent varchar(10),
+	  scrap_count varchar(10),
+	  scrap_percent varchar(10),
+	  downtime_minutes varchar(10)
+	);
 	
+	insert into results (primary_key,start_week,end_week,part_number,workcenter_code,planned_production_count,actual_production_count,actual_vrs_planned_percent,scrap_count,scrap_percent,downtime_minutes)
+	(
+			select
+			primary_key,
+			start_week,
+			end_week,
+			part_number,
+			workcenter_code,
+			format(planned_production_count, 'N0'),
+			format(actual_production_count, 'N0'),
+			CONVERT(varchar(10),actual_vrs_planned_percent) + '%' as actual_vrs_planned_percent,  
+			format(scrap_count, 'N0'),
+			CONVERT(varchar(10),scrap_percent) + '%' as scrap_percent,  
+			format(downtime_minutes, 'N0') + ' mins'
+			from
+			(
+				select
+				pk.primary_key,
+				CONVERT(VARCHAR, start_week ,101) start_week,
+				CONVERT(VARCHAR, end_week ,101) end_week,
+				--FORMAT ( pk.start_week, 'd', 'en-US' ) start_week, 
+				--FORMAT ( pk.end_week, 'd', 'en-US' ) end_week, 
+				pk.part_number,
+				pk.workcenter_code,
+				planned_production_count,
+				actual_production_count,
+				case
+				when planned_production_count = 0 then cast(0.00 as decimal(18,2))
+				else cast (actual_production_count*100./planned_production_count as decimal(18,2))
+				end actual_vrs_planned_percent, 
+				scrap_count,
+				case
+				when actual_production_count = 0 then cast(0.00 as decimal(18,2))
+				else cast (scrap_count*100./actual_production_count as decimal(18,2))
+				end scrap_percent, 
+				downtime_minutes 
+				from
+				(
+					select
+					primary_key,
+					sum(Hourly_planned_production_count) planned_production_count,
+					sum(Hourly_actual_production_count) actual_production_count,
+					sum(scrap_count) scrap_count,
+					floor(sum(Downtime_minutes)) Downtime_minutes 
+					from set2group 
+					group by primary_key
+				) sg
+				inner join primary_key pk
+				on sg.primary_key = pk.primary_key
+			)s1
+	)
+set pRecordCount = 5;
 
-/*		
-	Declare _start_year char(4)
-	Declare _start_week int
-	Declare @end_year char(4)
-	Declare @end_week int
-	Declare @start_of_week_for_start_date datetime
-	Declare @end_of_week_for_end_date datetime
-*/
-	set _record_count = 2;
-END 
--- https://www.mysqltutorial.org/mysql-drop-table/
-set @start_date = '2020-02-09T00:00:00';
-set @end_date = '2020-02-15T00:00:00';
-set @table_name = 'TempTable';
-
-CALL Kors.Sproc200206(@start_date, @end_date, @table_name,@rec);
-select @rec
+end;
+-- select * from HourlyOEEValues limit 100; 
+distinct date_time_stamp
+from HourlyOEEValues
+	set startWeekStartDate = FIRST_DAY_OF_WEEK(startDate);
+	set endWeekEndDate
+	
 
