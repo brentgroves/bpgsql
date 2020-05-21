e3d9a51c3cfb38343943d33f0cbd6e059bb5e328
Brent Groves <brent.groves@gmail.com>
2020-05-21 12:36:46 -0400

    Added/Update source code

--- kors/MySQL/Sproc200206.sql
+++ kors/MySQL/Sproc200206.sql
@@ -24,26 +24,28 @@ CREATE TABLE TempTable (
 
 
 
+-- set @startDate =STR_TO_DATE('12/31/2019 23:59:59','%m/%d/%Y %H:%i:%s'); -- week 52
 -- set @startDate = STR_TO_DATE('02/09/2020 00:00:00','%m/%d/%Y %H:%i:%s'); -- week 0
 -- set @startDate = STR_TO_DATE('02/15/2020 00:00:00','%m/%d/%Y %H:%i:%s'); -- week 0
--- set @startDate = STR_TO_DATE('03/01/2020 00:00:00','%m/%d/%Y %H:%i:%s'); -- week 0
-set @startDate = STR_TO_DATE('03/29/2020 00:00:00','%m/%d/%Y %H:%i:%s'); -- week 0
--- set @startDate =STR_TO_DATE('12/31/2019 23:59:59','%m/%d/%Y %H:%i:%s'); -- week 52
+set @startDate = STR_TO_DATE('03/01/2020 00:00:00','%m/%d/%Y %H:%i:%s'); -- week 0
+-- set @startDate = STR_TO_DATE('03/29/2020 00:00:00','%m/%d/%Y %H:%i:%s'); -- week 0
 -- set @startDate = STR_TO_DATE('01/04/2020 23:59:59','%m/%d/%Y %H:%i:%s'); -- week 0
 -- set @startDate = STR_TO_DATE('01/05/2020 23:59:59','%m/%d/%Y %H:%i:%s'); -- week 1
--- set @endDate = STR_TO_DATE('02/09/2020 00:00:00','%m/%d/%Y %H:%i:%s'); -- week 0
--- set @endDate = STR_TO_DATE('02/15/2020 00:00:00','%m/%d/%Y %H:%i:%s'); -- week 0
-set @endDate = STR_TO_DATE('04/04/2020 23:59:59','%m/%d/%Y %H:%i:%s'); -- week 52
 -- set @endDate =STR_TO_DATE('12/31/2019 23:59:59','%m/%d/%Y %H:%i:%s'); -- week 52
 -- set @endDate = STR_TO_DATE('01/04/2020 23:59:59','%m/%d/%Y %H:%i:%s'); -- week 0
 -- set @endDate = STR_TO_DATE('01/05/2020 23:59:59','%m/%d/%Y %H:%i:%s'); -- week 1
+-- set @endDate = STR_TO_DATE('02/09/2020 00:00:00','%m/%d/%Y %H:%i:%s'); -- week 0
+-- set @endDate = STR_TO_DATE('02/15/2020 00:00:00','%m/%d/%Y %H:%i:%s'); -- week 0
+-- set @startDate = STR_TO_DATE('03/01/2020 00:00:00','%m/%d/%Y %H:%i:%s'); -- week 0
+-- set @startDate = STR_TO_DATE('03/14/2020 00:00:00','%m/%d/%Y %H:%i:%s'); -- week 0
+ set @endDate = STR_TO_DATE('04/04/2020 23:59:59','%m/%d/%Y %H:%i:%s'); -- week 52
 set @tableName = 'TempTable';
 set @DEBUG = true;
 TRUNCATE TABLE debugger; 
 CALL Kors.Sproc200206(@startDate, @endDate, @tableName,@rec);
-SELECT * from debugger;
+-- SELECT * from debugger;
 SELECT @rec;
-
+select * from TempTable;
 
 DROP PROCEDURE Sproc200206;
 CREATE PROCEDURE Sproc200206
@@ -72,11 +74,12 @@ BEGIN
 	-- set @endDate ='2020-02-09 23:59:59';	
 	-- SET @tableName = 'TempTable';  -- Debug 
 -- */
-	SET @sql_query = CONCAT('DROP TABLE IF EXISTS ',@tableName);
-   	PREPARE stmt1 FROM @sql_query;
-	execute stmt1;
-	
-	set startWeek = STD_WEEK(startDate);
+	SET @sqlQuery = CONCAT('DROP TABLE IF EXISTS ',tableName);
+   	PREPARE stmt FROM @sqlQuery;
+	execute stmt;
+    DEALLOCATE PREPARE stmt;
+ 
+   	set startWeek = STD_WEEK(startDate);
 	set endWeek = STD_WEEK(endDate);
 
 	set startWeekStartDate = FIRST_DAY_OF_WEEK(startDate);
@@ -96,6 +99,7 @@ BEGIN
 	  part_number varchar(60),
 	  workcenter_code varchar(50)
 	);
+
 	insert into primary_key(primary_key,year_week,year_week_fmt,start_week,end_week,part_number,workcenter_code)
 	(
 	  select 
@@ -135,6 +139,7 @@ BEGIN
 		scrap_count int,
 		Downtime_minutes float
 	);
+
 	insert into set2group (primary_key,Hourly_planned_production_count,Hourly_actual_production_count,scrap_count,Downtime_minutes)
 	(
 	select
@@ -167,7 +172,8 @@ BEGIN
 	create temporary table results
 	(
 	  primary_key int,
-	  start_week varchar(12),
+  	  year_week_fmt varchar(10),
+  	  start_week varchar(12),
 	  end_week varchar(12),
 	  part_number varchar(60),
 	  workcenter_code varchar(50),
@@ -178,65 +184,88 @@ BEGIN
 	  scrap_percent varchar(10),
 	  downtime_minutes varchar(10)
 	);
-	
-	insert into results (primary_key,start_week,end_week,part_number,workcenter_code,planned_production_count,actual_production_count,actual_vrs_planned_percent,scrap_count,scrap_percent,downtime_minutes)
+	insert into results (primary_key,year_week_fmt,start_week,end_week,part_number,workcenter_code,planned_production_count,actual_production_count,actual_vrs_planned_percent,scrap_count,scrap_percent,downtime_minutes)
 	(
+		select
+		primary_key,
+		year_week_fmt,
+		start_week,
+		end_week,
+		part_number,
+		workcenter_code,
+		format(planned_production_count, 0) as planned_production_count,  
+		format(actual_production_count, 0) as actual_production_count,
+		-- CONVERT(varchar(10),actual_vrs_planned_percent) + '%' as actual_vrs_planned_percent,  
+		concat(format(actual_vrs_planned_percent,2),'%') as actual_vrs_planned_percent, 
+		format(scrap_count,0) as scrap_count,
+		concat(format(scrap_percent,2),'%') as scrap_percent, 
+		concat(format(downtime_minutes, 0),' mins') as downtime_minutes
+		from
+		(
 			select
-			primary_key,
-			start_week,
-			end_week,
-			part_number,
-			workcenter_code,
-			format(planned_production_count, 'N0'),
-			format(actual_production_count, 'N0'),
-			CONVERT(varchar(10),actual_vrs_planned_percent) + '%' as actual_vrs_planned_percent,  
-			format(scrap_count, 'N0'),
-			CONVERT(varchar(10),scrap_percent) + '%' as scrap_percent,  
-			format(downtime_minutes, 'N0') + ' mins'
+			pk.primary_key,
+			pk.year_week_fmt,
+			DATE_FORMAT(start_week,'%m/%d/%Y') start_week,
+			DATE_FORMAT(end_week,'%m/%d/%Y') end_week,
+			pk.part_number,
+			pk.workcenter_code,
+			planned_production_count,
+			actual_production_count,
+			case
+			when planned_production_count = 0 then cast(0.00 as decimal(18,2))
+			else cast(actual_production_count*100/planned_production_count as decimal(18,2))
+			end actual_vrs_planned_percent, 
+			scrap_count,
+			case
+			when actual_production_count = 0 then cast(0.00 as decimal(18,2))
+			else cast(scrap_count*100/actual_production_count as decimal(18,2))
+			end scrap_percent, 
+			downtime_minutes 
 			from
 			(
-				select
-				pk.primary_key,
-				CONVERT(VARCHAR, start_week ,101) start_week,
-				CONVERT(VARCHAR, end_week ,101) end_week,
-				--FORMAT ( pk.start_week, 'd', 'en-US' ) start_week, 
-				--FORMAT ( pk.end_week, 'd', 'en-US' ) end_week, 
-				pk.part_number,
-				pk.workcenter_code,
-				planned_production_count,
-				actual_production_count,
-				case
-				when planned_production_count = 0 then cast(0.00 as decimal(18,2))
-				else cast (actual_production_count*100./planned_production_count as decimal(18,2))
-				end actual_vrs_planned_percent, 
-				scrap_count,
-				case
-				when actual_production_count = 0 then cast(0.00 as decimal(18,2))
-				else cast (scrap_count*100./actual_production_count as decimal(18,2))
-				end scrap_percent, 
-				downtime_minutes 
-				from
-				(
-					select
-					primary_key,
-					sum(Hourly_planned_production_count) planned_production_count,
-					sum(Hourly_actual_production_count) actual_production_count,
-					sum(scrap_count) scrap_count,
-					floor(sum(Downtime_minutes)) Downtime_minutes 
-					from set2group 
-					group by primary_key
-				) sg
-				inner join primary_key pk
-				on sg.primary_key = pk.primary_key
-			)s1
-	)
-set pRecordCount = 5;
 
-end;
+				select
+				primary_key,
+				sum(Hourly_planned_production_count) planned_production_count,
+				sum(Hourly_actual_production_count) actual_production_count,
+				sum(scrap_count) scrap_count,
+				floor(sum(Downtime_minutes)) Downtime_minutes 
+				from set2group 
+				group by primary_key
+			) sg
+			inner join primary_key as pk
+			on sg.primary_key = pk.primary_key
+		) s2
+	);	
+	set @sqlQuery = CONCAT('create table ',tableName,' select * from results order by primary_key');
+	PREPARE stmt FROM @sqlQuery;
+	execute stmt;
+    DEALLOCATE PREPARE stmt;
+
+   	-- SELECT ROW_COUNT(); -- 0
+   	set pRecordCount = FOUND_ROWS();
+end;	
+
+	declare @sql nvarchar(4000)
+	select @sql = N'SELECT * into ' + quotename(@table_name) + N' from #results order by primary_key'
+	
+	--select @sql = N'SELECT start_week,end_week,part_number,workcenter_code,planned_production_count,actual_production_count,actual_vrs_planned_percent,scrap_count,downtime_minutes into ' + quotename(@table_name) + N' from #results order by primary_key'
+	
+	EXEC sp_executesql @sql
+	    
+	SELECT @record_count = @@ROWCOUNT;
 -- select * from HourlyOEEValues limit 100; 
 distinct date_time_stamp
 from HourlyOEEValues
 	set startWeekStartDate = FIRST_DAY_OF_WEEK(startDate);
 	set endWeekEndDate
 	
+/*	  
+set @actual_production_count = 100;
+set @planned_production_count = 110;
+set @m = @actual_production_count*100/@planned_production_count;
+select cast(@actual_production_count*100/@planned_production_count as decimal(18,2));
+select cast(@m as decimal(18,2));
+*/
+-- select cast ((@actual_production_count*100/@planned_production_count) as decimal(18,2));
 
