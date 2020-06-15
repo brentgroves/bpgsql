735fec86a21d1bf785a72f2a6f657efd0c7a5a8b
Brent Groves <brent.groves@gmail.com>
2020-06-15 10:21:18 -0400

    Added/Changed Code

--- functions/mysql/functions.sql
+++ functions/mysql/functions.sql
@@ -32,6 +32,7 @@ BEGIN
 	-- IF @DEBUG then
 	--   INSERT INTO debugger VALUES (CONCAT('[FIRST_DAY_OF_WEEK: day=',day,',week=',week,',firstDay=',firstDay,']'));
 	-- End If;
+	
 	return firstDay;
 END;	
 
@@ -121,4 +122,23 @@ BEGIN
 
 	return week;
 END;
-  
+
+CREATE PROCEDURE check_table_exists(table_name VARCHAR(100)) 
+BEGIN
+    DECLARE CONTINUE HANDLER FOR SQLSTATE '42S02' SET @err = 1;
+    SET @err = 0;
+    SET @table_name = table_name;
+    SET @sql_query = CONCAT('SELECT 1 FROM ',@table_name);
+    PREPARE stmt1 FROM @sql_query;
+   	execute b;
+    IF (@err = 1) THEN
+        SET @table_exists = 0;
+    ELSE
+        SET @table_exists = 1;
+        DEALLOCATE PREPARE stmt1;
+    END IF;
+   select 1 from HourlyOEEValues ho 
+END 
+
+call check_table_exists('HourlyOEEValues');
+
