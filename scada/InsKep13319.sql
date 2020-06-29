8f996039650f9bf48d3a4e83cc6822e498c88c2c
Brent Groves <brent.groves@gmail.com>
2020-06-29 14:39:49 -0400

    Added/Changed Code

--- scada/InsKep13319.sql
+++ scada/InsKep13319.sql
@@ -1,16 +1,16 @@
 -- drop table Kep13319
 CREATE TABLE Kep13319 (
-	Kep13319_Key int(11) NOT NULL AUTO_INCREMENT,
-  	NodeId varchar(50) DEFAULT NULL,
-  	Name varchar(50) DEFAULT NULL,
-  	Plexus_Customer_No int(11) DEFAULT NULL,
-	PCN varchar(50) DEFAULT NULL,
-  	Workcenter_Key int(11) DEFAULT NULL,
-  	Workcenter_Code varchar(50) DEFAULT NULL,
-  	CNC varchar(6) DEFAULT NULL,
-  	Value int(11) DEFAULT NULL,
-  	TransDate datetime DEFAULT NULL,
-  	PRIMARY KEY (Kep13319_Key)
+	kep13319Key int(11) NOT NULL AUTO_INCREMENT,
+  	nodeId varchar(50) DEFAULT NULL,
+  	name varchar(50) DEFAULT NULL,
+  	plexus_Customer_No int(11) DEFAULT NULL,
+	pcn varchar(50) DEFAULT NULL,
+  	workcenter_Key int(11) DEFAULT NULL,
+  	workcenter_Code varchar(50) DEFAULT NULL,
+  	cnc varchar(6) DEFAULT NULL,
+  	value int(11) DEFAULT NULL,
+  	transDate datetime DEFAULT NULL,
+  	PRIMARY KEY (Kep13319Key)
 ) ENGINE=InnoDB AUTO_INCREMENT=17 DEFAULT CHARSET=utf8mb4 COMMENT='OPC Client historian';
 
 set @transDate = '2020-06-25 00:00:00';
@@ -32,11 +32,11 @@ CREATE PROCEDURE InsKep13319
 BEGIN
    
 INSERT INTO Kep13319 
-(NodeId,Name,Plexus_Customer_No,PCN, Workcenter_Key,Workcenter_Code,CNC,Value,TransDate)
+(nodeId,name,plexus_Customer_No,pcn, workcenter_Key,workcenter_Code,cnc,value,transDate)
 VALUES(nodeId,name,plexus_Customer_No,pcn, workcenter_Key,workcenter_Code,cnc,value,transDate);
 
 -- Display the last inserted row.
-select Kep13319_Key, Workcenter_Key from Kep13319 k where Kep13319_Key=(SELECT LAST_INSERT_ID());
+select kep13319Key, workcenter_Key from Kep13319 k where Kep13319Key=(SELECT LAST_INSERT_ID());
 
 END;
 
