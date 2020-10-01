4fbe6a50040d544ce9386d6630a83df0c2206cb7
Brent Groves <brent.groves@gmail.com>
2020-10-01 16:02:08 -0400

    Initial Creation

--- CM/ItemLocationDiff.sql
+++ CM/ItemLocationDiff.sql
@@ -1,9 +1,10 @@
 -- DO drops later in case you spot an error somewhere else in the process
 -- I messed up and called 1223 files 1213.
 select * 
-into station092820
+into station100120
 from STATION
-select count(*) cnt from station092820
+select count(*) cnt from station100120
+--12679 10/01
 --12679 09/28
 --12679 09/21
 --12679 09/14
@@ -44,17 +45,17 @@ select count(*) cnt from station092820
 --12624
 --verify backup of station
 
-select top 100 * from station092820
+select top 100 * from station100120
 -- Upload the item_location table into PlxSupplyItemLocation table.
-CREATE TABLE Cribmaster.dbo.PlxSupplyItemLocation092820 (
+CREATE TABLE Cribmaster.dbo.PlxSupplyItemLocation100120 (
 	item_no varchar(50),
 	location varchar(50),
 	quantity integer
 )
 
 -- Insert Plex item_location data into CM
-Bulk insert PlxSupplyItemLocation092820
-from 'c:\il0928GT12500.csv'
+Bulk insert PlxSupplyItemLocation100120
+from 'c:\il1001GT12500.csv'
 with
 (
 	fieldterminator = ',',
@@ -65,13 +66,14 @@ with
 select
 count(*)
 --top 1000 * 
-from PlxSupplyItemLocation092820 
+from PlxSupplyItemLocation100120 
 -- Check for duplicates
 select count(*)
 from 
 (
-select distinct item_no,location from PlxSupplyItemLocation092820
+select distinct item_no,location from PlxSupplyItemLocation100120
 )s1
+-- 14570 10/01
 -- 14547 09/28
 -- 14487 09/21
 -- 14547 09/14
@@ -98,10 +100,11 @@ select distinct item_no,location from PlxSupplyItemLocation092820
 
 
 select 
-il.item_no
+ il.item_no
 --il.item_no,il.location,il.quantity
- into nic092820 --Plex supply items with the default location and a quantity = 0
+ into nic100120 --Plex supply items with the default location and a quantity = 0
 -- count(*) 
+ --3783 10/01
  --3773 09/28
 --3714 09/21
 --3685 09/09
@@ -121,7 +124,7 @@ il.item_no
 from (
 	select --distinct incase I inserted items more than once
 		distinct item_no,location,quantity
-	from PlxSupplyItemLocation092820
+	from PlxSupplyItemLocation100120
 ) il
 left outer join STATION st 
 on il.location=st.CribBin
@@ -138,7 +141,8 @@ and il.quantity = 0
 --and il.quantity <> 0 
 
 	select COUNT(*)
-	from nic092820
+	from nic100120
+	--3783 10/01
 	--3771 09/28
 	--3714 09/21
 	--3685 09/14
@@ -192,7 +196,8 @@ and il.quantity = 0
 set BinQuantity = 0,
 Quantity = 0
 --select 
--- count(*) cnt 
+-- count(*) cnt
+--10/01 11
 --09/08 11
 --07/02 14
 --05/18 11 items
@@ -203,9 +208,10 @@ Quantity = 0
 	item in 
 	(
 	select item_no
-	from nic092820
+	from nic100120
 	)
 	and (st.BinQuantity<>0 or st.Quantity <> 0 )
+	-- 11 10/01/20
 	-- 11 09/28/20
 	-- 11 09/21/20
 	-- 11 09/14/20
@@ -234,20 +240,20 @@ Quantity = il.quantity
 --select 
 --il.item_no
 --il.item_no,il.location,il.quantity
---count(*) 
+-- count(*) 
 from (
 	select --distinct incase I inserted items more than once
 		distinct item_no,location,quantity
-	from PlxSupplyItemLocation092820
+	from PlxSupplyItemLocation100120
 ) il
 inner join STATION st 
 on il.location=st.CribBin
 and il.item_no=st.Item
 --0729=10630, 0726=10630, --0628=11285
 where 
- il.quantity <> st.BinQuantity --267 09/28, 291, 09/21,184 09/14,212 09/08, 581 09/01,158 08/17,261 07/20, 246 07/13, 386 07/01,309 06/30, 179 06/15,06/09 333,06/02 89,06/01 126, --05/26=115,05/18=172,340,334,351,381,421,375,304,409,312, 213,177,1330,1319,510,293,376,416,417,472, 342,353, 455,406,384	
--- il.quantity > st.BinQuantity --142 09/21,74 09/14,77 09/08, 211 09/01,48 08/17,50 07/20, 148 07/01,112 06/30,60 06/22,43 06/15,145 06/09,12 06/02,58 06/01 05/26=40,05/18=84,120,143,107,95,154,149,122, 124, 124,77,51, 492,538,134,140,171,172,177,120,138,131,61
--- il.quantity < st.BinQuantity --125 09/28,167 09/21,110 09/14,135 09/11, 370 09/01,110 08/17/20,211 07/20,173 07/13,238 07/01, 197 06/30,190 06/22,136 06/15,188 06/09, 77 06/02 --68 06/01--05/26=75,05/18=88,220,191,244,286,267,226,182,285,188,136,126,838,781,376,168,236,245,245,295,222,311,215, 316,275,105
+ il.quantity <> st.BinQuantity --247 10/01, 267 09/28, 291, 09/21,184 09/14,212 09/08, 581 09/01,158 08/17,261 07/20, 246 07/13, 386 07/01,309 06/30, 179 06/15,06/09 333,06/02 89,06/01 126, --05/26=115,05/18=172,340,334,351,381,421,375,304,409,312, 213,177,1330,1319,510,293,376,416,417,472, 342,353, 455,406,384	
+-- il.quantity > st.BinQuantity --112 10/01,142 09/21,74 09/14,77 09/08, 211 09/01,48 08/17,50 07/20, 148 07/01,112 06/30,60 06/22,43 06/15,145 06/09,12 06/02,58 06/01 05/26=40,05/18=84,120,143,107,95,154,149,122, 124, 124,77,51, 492,538,134,140,171,172,177,120,138,131,61
+ --il.quantity < st.BinQuantity --135 10/01,125 09/28,167 09/21,110 09/14,135 09/11, 370 09/01,110 08/17/20,211 07/20,173 07/13,238 07/01, 197 06/30,190 06/22,136 06/15,188 06/09, 77 06/02 --68 06/01--05/26=75,05/18=88,220,191,244,286,267,226,182,285,188,136,126,838,781,376,168,236,245,245,295,222,311,215, 316,275,105
 --80 more items dropped in quantity 0820
 --385
 --0813=389
--- toollist/bvToolListItemsOnlyLv1.sql
+++ toollist/bvToolListItemsOnlyLv1.sql
@@ -16,15 +16,19 @@ where processid = 61748
  and Consumable = 1
 order by toolNumber,toolType
 
-select tt.ToolNumber,tt.OpDescription,tt.Alternate,ti.CribToolID,ti.QuantityPerCuttingEdge,ti.AdditionalNotes
+select tt.ToolNumber TN,ti.CribToolID CribId,ti.Quantity QtyReq,ti.NumberOfCuttingEdges Edges,ti.QuantityPerCuttingEdge TLife
+-- ti.AdditionalNotes
+-- ,ti.*
+-- tt.OpDescription,tt.Alternate,ti.CribToolID,ti.QuantityPerCuttingEdge,ti.AdditionalNotes
 -- ti.*
 from [ToolList Tool] tt
 inner join [ToolList Item] ti
 on tt.processid = ti.processid 
 and tt.ToolID = ti.ToolID
 and ti.Consumable = 1
-and tt.ToolNumber = 12
-where tt.processid = 61748
+-- and tt.ToolNumber = 12
+-- where tt.processid = 61748 -- P558 Knuckles
+where tt.processid = 61442 -- RDX 51393TJB A040M1
 order by tt.ToolNumber
 
 61748|10103355 
