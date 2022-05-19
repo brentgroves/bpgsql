0a8a34fcd1d323298c2ac97cb88bbc2ef52cd750
Brent Groves <bgroves@buschegroup.com>
2022-05-19 12:41:37 -0400

    update

---
+++ DW/Plex/ReportCreationProcess.sql
@@ -0,0 +1,13 @@
+Create a Power BI report from Plex 
+1. Create a Plex Stored Procedure or identify a Plex web service with the data you need.
+2. Create a Azure automation ETL script or SSIS ETL script to transfer the date from Plex to the data warehouse. Can ask Sam or Brent to do this.
+3. Create a sql view in the data warehouse.
+4. Create a Power BI report using the data warehouse sql view.
+5. Publish the Power BI report to the Power BI test or live workspace. Can ask Sam or Brent to do this.
+6. Add the report to a Microsoft teams tab. Can ask Sam or Brent to do this.
+
+All you need to do is.
+1. Install Power BI desktop on your laptop.
+2. Create the Plex stored procedure or identify a web service with the data you need.
+3. Create a sql view in the data warehouse.
+4. Create a Power BI report using the data warehouse sql view.
