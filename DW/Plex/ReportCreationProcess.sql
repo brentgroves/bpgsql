b7e2739250600ff46ce4919021e85f2643fb04a0
Brent Groves <bgroves@buschegroup.com>
2022-05-20 13:59:49 -0400

    update

--- DW/Plex/ReportCreationProcess.sql
+++ DW/Plex/ReportCreationProcess.sql
@@ -11,3 +11,22 @@ All you need to do is.
 2. Create the Plex stored procedure or identify a web service with the data you need.
 3. Create a sql view in the data warehouse.
 4. Create a Power BI report using the data warehouse sql view.
+
+Added benefit of Power BI reporting language.
+You can set filters on specific columns.
+
+Yes, Go to https://test.plexonline.com, You have to go Directly to this site. You can't switch databases from production. And you must have the Cumulus extension installed. 
+
+Yes, Go to https://test.plexonline.com, You have to go Directly to this site. You can't switch databases from production. And you must have the Cumulus extension installed. 
+Data warehouse connection string
+mgsqlmi.public.48d444e7f69b.database.windows.net,3342
+mgadmin
+WeDontSharePasswords1!
+
+Need access to All Reports
+
+create procedure Plex.accounting_account_sproc
+as 
+
+create view Plex.accounting_account_view 
+as 
